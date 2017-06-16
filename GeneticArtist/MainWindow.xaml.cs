1776e350127a7ab5c01ec227562a9dabcdeb3635
Adam Stirtan <adamstirtan@gmail.com>
2017-06-15 21:33:03 -0400

    Implemented basic image loading in WPF

---
+++ GeneticArtist/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Drawing.Imaging;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+using Microsoft.Win32;
+
+namespace GeneticArtist
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonOpen_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog
+            {
+                Filter = ""
+            };
+
+            var codecs = ImageCodecInfo.GetImageEncoders();
+            var separator = string.Empty;
+
+            foreach (var codec in codecs)
+            {
+                var name = codec.CodecName.Substring(8).Replace("Codec", "Files").Trim();
+
+                dialog.Filter = string.Format("{0}{1}{2} ({3})|{3}",
+                    dialog.Filter,
+                    separator,
+                    name,
+                    codec.FilenameExtension);
+
+                separator = "|";
+            }
+
+            dialog.Filter = string.Format("{0}{1}{2} ({3})|{3}",
+                dialog.Filter,
+                separator,
+                "All Files",
+                "*.*");
+
+            if (dialog.ShowDialog() == true)
+            {
+                ImageSource.Source = new BitmapImage(new Uri(dialog.FileName));
+            }
+        }
+    }
+}
